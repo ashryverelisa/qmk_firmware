98dae499f146060423c770a483edbca7cb30bb91
AshryverElisa <elisaashryver@hotmail.com>
2026-06-02 18:12:04 +0200

    begin second layer

--- keyboards/keebart/sofle_choc_pro/keymaps/ashryver/keymap.c
+++ keyboards/keebart/sofle_choc_pro/keymaps/ashryver/keymap.c
@@ -1,11 +1,8 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
-
 #include QMK_KEYBOARD_H
 
 enum layers {
     BASE,  // default layer
-    MDIA,  // media keys
+    LOWER, // lower layer
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -29,29 +26,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                           KC_Y,     KC_U,     KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_CAPS,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                           KC_H,     KC_J,     KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,   KC_MPLY,    KC_N,     KC_M,     KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                 KC_LCTL, KC_LALT, KC_LCMD, LT(MDIA,KC_LGUI), KC_ENT,  KC_SPC, LT(MDIA,KC_RGUI),  KC_DEL,  KC_RALT, KC_RCTL
+                 KC_LCTL, KC_LALT, KC_LCMD, LT(LOWER,KC_LGUI), KC_ENT,  KC_SPC, LT(LOWER,KC_RGUI),  KC_DEL,  KC_RALT, KC_RCTL
 ),
 
 /*
  * QWERTY
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | MUTE |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Togg |      |      |      |      |      |                    |      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |      |      |      |      |      | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Next |      |      |      |      |      |-------.    ,-------|      |      |      |      |      |      |
+ * |      |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * | HuEu |      |      |      |      |      |-------|    |-------|      |      |      |      |      |      |
+ * |      |  =   |  -   |  +   |   {  |   }  |-------|    |-------|   [  |   ]  |   ;  |   :  |   \  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            |      |      |      |      | /       /       \      \  |      |      |      |      |
+ *            |      |      |      |      | /Enter  /       \Space \  |      |      |      |      |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
-[MDIA] = LAYOUT_split_4x6_5(
-    KC_MUTE,_______,_______,_______,_______,_______,                           _______,_______,_______,_______,_______,_______,
-    RM_TOGG,_______,_______,_______,_______,_______,                           _______,_______,_______,_______,_______,_______,
-    RM_NEXT,_______,_______,_______,_______,_______,                           _______,_______,_______,_______,_______,_______,
-    RM_HUEU,_______,_______,_______,_______,_______,   _______,     _______,   _______,_______,_______,_______,_______,_______,
+[LOWER] = LAYOUT_split_4x6_5(
+    _______,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,                             KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11,
+    _______,_______,_______,_______,_______,_______,                           _______,_______,_______,_______,_______, KC_F12,
+    _______,KC_EXLM,  KC_AT,KC_HASH, KC_DLR,KC_PERC,                           KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_PIPE,
+    _______, KC_EQL,KC_MINS,KC_PLUS,KC_LCBR,KC_RCBR,   _______,     _______,   KC_LBRC,KC_RBRC,KC_SCLN,KC_COLN,KC_BSLS,_______,
                     _______,_______,_______,_______,   _______,     _______,   _______,_______,_______,_______
 )
 };
@@ -59,6 +56,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU), ENCODER_CCW_CW(KC_MPRV, KC_MNXT) },
-    [MDIA] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) }
+    [LOWER] = { ENCODER_CCW_CW(_______, _______), ENCODER_CCW_CW(_______, _______) }
 };
 #endif
